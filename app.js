4f0f5e8f0c9567094659457d9934509569363f8f
Lukas Simianer <lsimianer@gmail.com>
2020-03-23 18:06:39 -0500

    basics in functionality to come

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Im allllliveeee');
+
+const bestFitCalc = () =>{
+    let waistSize = document.getElementById('waistSizeSelector').value;
+    let inseam = document.getElementById('inseamSelector').value
+    console.log('waistSize :' + waistSize);
+    console.log('inseam :' + inseam);
+
+}
+
+/*
+Large = 38-42W && 16-18I
+XL = 44-48w 20-22-I
+XXL =50-52 W
+
+*/

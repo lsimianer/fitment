b313ad5b81db8e280f1a9e18894aa30096e68602
Lukas Simianer <lsimianer@gmail.com>
2020-03-24 10:32:25 -0500

    fitment metrics in, functionality now

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 console.log('Im allllliveeee');
 
 const bestFitCalc = () =>{
+    let height = document.getElementById('heightSelector').value
     let waistSize = document.getElementById('waistSizeSelector').value;
     let inseam = document.getElementById('inseamSelector').value
+    let neck = document.getElementById('neckSizeSelector').value
+    let arms = document.getElementById('armsSizeSelector').value
+    let chest = document.getElementById('ChestSizeSelector').value
+    let email = document.getElementById('bestFitEmail').value
+    console.log('height :' + height);
     console.log('waistSize :' + waistSize);
     console.log('inseam :' + inseam);
+    console.log('neck :' + neck);
+    console.log('arms :' + arms);
+    console.log('chest :' + chest);
+    console.log('email :' + email);
 
 }
 
